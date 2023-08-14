ecb750decc1189cf1da4481dcdb0e618f4edaa9f
isrbrepin <isrbrepin@alum.us.es>
2023-08-14 18:27:40 +0200

    Initial commit

---
+++ app/src/main/java/es/us/myapptfg/MainActivity.kt
@@ -0,0 +1,11 @@
+package es.us.myapptfg
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
