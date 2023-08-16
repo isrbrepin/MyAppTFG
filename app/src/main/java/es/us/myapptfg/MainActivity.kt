4febcb3a485e1470248f71c53b1c9197d858223e
isrbrepin <isrbrepin@alum.us.es>
2023-08-16 16:55:39 +0200

    Primera Pagina del proyecto. Todo un exito.

--- app/src/main/java/es/us/myapptfg/MainActivity.kt
+++ app/src/main/java/es/us/myapptfg/MainActivity.kt
@@ -2,10 +2,14 @@ package es.us.myapptfg
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.FrameLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Puedes realizar acciones con el nuevo FrameLayout aquí
+        // Agrega fragmentos, vistas u otros elementos dentro del FrameLayout
     }
 }
