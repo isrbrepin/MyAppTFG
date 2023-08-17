076862f6eddc768f74080f2d790abef9650350db
isrbrepin <isrbrepin@alum.us.es>
2023-08-17 21:28:45 +0200

    Pantalla info proceso terminado

--- app/src/main/java/es/us/myapptfg/MainActivity.kt
+++ app/src/main/java/es/us/myapptfg/MainActivity.kt
@@ -1,11 +1,10 @@
 package es.us.myapptfg
 
 import android.content.Intent
-import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.FrameLayout
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +14,9 @@ class MainActivity : AppCompatActivity() {
         // Puedes realizar acciones con el nuevo FrameLayout aquí
         // Agrega fragmentos, vistas u otros elementos dentro del FrameLayout
     }
-    fun openActivityInfo(view: View) {
+
+
+    fun openInfoActivity(view: View) {
         val intent = Intent(this, InfoActivity::class.java)
         startActivity(intent)
     }
